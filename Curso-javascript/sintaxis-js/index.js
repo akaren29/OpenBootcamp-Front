036a3766853a479a63e7a6f5dfcc5cba1e8e6a78
akaren29 <akjassoj@outlook.com>
2022-10-26 12:23:56 -0500

    Ejercicio sintaxis y variables de js

---
+++ Curso-javascript/sintaxis-js/index.js
@@ -0,0 +1,9 @@
+let name = "karen";
+let age = 31;
+let isDeveloper = true;
+let date = "29/05/1991";
+const bookFavorite = {
+    title: "El universo en tus manos",
+    author: "Christophe Galfard",
+    url : "https://www.buscalibre.com.mx/libro-el-universo-en-tus-manos-sonia-fernandez-vidal-la-galera-s-a-editorial/9788424653422/p/46876918?gclid=Cj0KCQjwteOaBhDuARIsADBqReiQQlqE2Ju0Iacaq2Mz0P_aABXl1JnBfcTPyJ9g9VhPP2FmKtCBajUaAsXsEALw_wcB"
+}
