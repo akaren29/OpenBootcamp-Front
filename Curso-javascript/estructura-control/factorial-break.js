b83feca5e0567e1466b0d8b592dd436bca865a1c
akaren29 <akjassoj@outlook.com>
2022-11-07 22:24:50 -0600

    Ejercicio estructuras de control

---
+++ Curso-javascript/estructura-control/factorial-break.js
@@ -0,0 +1,14 @@
+let resultado = 1;
+let numero= 1;
+
+console.log(resultado);
+if(numero == 0){
+    return 1
+}
+
+while(numero <= 10){
+    resultado = resultado * numero;
+    numero++;
+    console.log(resultado);
+}
+
