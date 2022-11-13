d6e4435de2d25882b7709a74ef1220371b5a66b7
akaren29 <akjassoj@outlook.com>
2022-11-13 13:51:20 -0600

    Ejercicio de numeros

---
+++ Curso-javascript/numbers/numbers.js
@@ -0,0 +1,15 @@
+let altura = 156;;
+let alturaFloat = 1.56;
+let peso = 62.300;
+
+let alturaMetros = Math.round(altura);
+console.log(alturaMetros);
+
+let pesoKg = Math.floor(peso);
+console.log(pesoKg);
+
+let maxValor = Number.MAX_VALUE + 1;
+console.log(maxValor);
+
+let esIgualMaxValor = maxValor === Number.MAX_VALUE;
+console.log(esIgualMaxValor);
