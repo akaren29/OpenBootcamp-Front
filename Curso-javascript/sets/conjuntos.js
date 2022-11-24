c75501fed346371cf3a80604b1fa9e5d56c09e34
akaren29 <akjassoj@outlook.com>
2022-11-23 22:43:56 -0600

    Ejercicio de conjunto de sets

---
+++ Curso-javascript/sets/conjuntos.js
@@ -0,0 +1,9 @@
+const setFamily = new Set(['Fernando', 'Antonia', 'Melany',
+'edgar',  'estefany','esteban']);
+console.log(setFamily);
+
+setFamily.add('karen');
+console.log(setFamily);
+
+setFamily.add('javascript');
+console.log(setFamily);
