44afebd20dc97096f5c8c8b4e4eebd3db882d7d4
akaren29 <akjassoj@outlook.com>
2023-01-09 11:52:13 -0600

    ejercicio con jquery

---
+++ Curso-javascript/jquery/script.js
@@ -0,0 +1,9 @@
+const boton = document.querySelector('#btn')
+
+/*boton.addEventListener('click', ()=>{
+    alert('Click en el navegador')
+})*/
+
+$("#btn").click( ()=>{
+    console.log("Hola, estoy utilizando jQuery");
+})
