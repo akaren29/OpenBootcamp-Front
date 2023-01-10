6169a7068b231e6263c424004d8765ac92b5bbe7
akaren29 <akjassoj@outlook.com>
2023-01-09 23:17:57 -0600

    ejercicio de persistencia de datos

---
+++ Curso-javascript/persistencia/index.js
@@ -0,0 +1,15 @@
+let nombre = "ana";
+let apellido = "jassp";
+
+const info = {
+    nombre: "ana",
+    apellido: "jasso"
+}
+
+/*  localStorage.setItem("persona",info)
+sessionStorage.setItem("persona",info)
+
+document.cookie = "nombreCookie="+info;
+document.cookie = "nombreCaducidad=Nombre;expires= 10 JAN 2023 23:13:00 GMT";  */
+
+console.log(document.cookie);
